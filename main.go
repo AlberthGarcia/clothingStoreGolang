0be890b25fc3e253549a5d1f0b01132a7dfcacbf
AlberthGarcia <alberthperezgarcia@gmail.com>
2022-01-23 23:43:17 -0600

    New funct and templates
    Func to render templates and to modify clothing

--- main.go
+++ main.go
@@ -14,9 +14,13 @@ func main() {
 	//mux
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", Index)
-	mux.HandleFunc("/seeClothing", ListClothing)
 	mux.HandleFunc("/addClothing", addClothing)
+	mux.HandleFunc("/seeClothing", listClothing)
 	mux.HandleFunc("/insertClothing", models.InsertClothing)
+	mux.HandleFunc("/searchClothing", searchClothing)
+	mux.HandleFunc("/updatedClothes", updatedClothing)
+	mux.HandleFunc("/deleteClothing", models.DeleteClothing)
+	mux.HandleFunc("/updatedClothing", models.UpdatedClothes)
 
 	//server
 	server := &http.Server{
@@ -29,18 +33,32 @@ func main() {
 }
 
 //Execute the template and send the struct to the html
-func renderTemplate(rw http.ResponseWriter, name string) {
-	temp.ExecuteTemplate(rw, name, nil)
+func renderTemplate(rw http.ResponseWriter, name string, data interface{}) {
+	err := temp.ExecuteTemplate(rw, name, data)
+	if err != nil {
+		panic(err.Error())
+	}
 }
 
 func Index(rw http.ResponseWriter, r *http.Request) {
-	renderTemplate(rw, "index.html")
+	renderTemplate(rw, "index.html", nil)
 }
 
-func ListClothing(rw http.ResponseWriter, r *http.Request) {
-	renderTemplate(rw, "seeClothing.html")
+func listClothing(rw http.ResponseWriter, r *http.Request) {
+	list := models.ListClothing()
+	renderTemplate(rw, "seeClothing.html", list)
 }
 
 func addClothing(rw http.ResponseWriter, r *http.Request) {
-	renderTemplate(rw, "addClothing.html")
+	renderTemplate(rw, "addClothing.html", nil)
+}
+
+func searchClothing(rw http.ResponseWriter, r *http.Request) {
+	listClothing := models.SearchClothingbyName(r)
+	renderTemplate(rw, "searchClothing.html", listClothing)
+}
+
+func updatedClothing(rw http.ResponseWriter, r *http.Request) {
+	clothes := models.UpdatedClothing(r)
+	renderTemplate(rw, "updatedClothes.html", clothes)
 }
--- models/clothing.go
+++ models/clothing.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"fmt"
 	"net/http"
 	"pack/db"
 	"strconv"
@@ -14,6 +15,8 @@ type Clothing struct {
 	CustomerCost float64
 	Existence    int64
 }
+
+type Clothes []Clothing
 
 //Construct of Clothing
 func buildClothing(name, breed string, cost, customercost float64, existence int64) Clothing {
@@ -73,6 +76,128 @@ func InsertClothing(rw http.ResponseWriter, r *http.Request) {
 		defer responsePrepare.Close()
 	}
 
-	http.Redirect(rw, r, "/", http.StatusMovedPermanently)
+	http.Redirect(rw, r, "/seeClothing", http.StatusMovedPermanently)
+
+}
+
+//FUNC TO LIST THE CLOTHIN
+func ListClothing() Clothes {
+	connection := db.OpenConnection()
+	sql := "SELECT * FROM productos"
+	rowsQueryClothing, err := connection.Query(sql)
+	if err != nil {
+		panic(err.Error())
+	}
+	clothing := Clothes{}
+	for rowsQueryClothing.Next() {
+		clothes := Clothing{}
+		rowsQueryClothing.Scan(&clothes.Id, &clothes.Name, &clothes.Breed, &clothes.Cost, &clothes.CustomerCost, &clothes.Existence)
+
+		clothing = append(clothing, clothes)
+	}
+
+	return clothing
+}
+
+//FUNC TO SEARCH CLORHING BY  NAME
+func SearchClothingbyName(r *http.Request) (clothing Clothes) {
+	conexion := db.OpenConnection()
+
+	if r.Method == "POST" {
+		nameProduct := r.FormValue("search")
+		sql := "SELECT * FROM productos where name=?"
+
+		rowsQueryClothing, err := conexion.Query(sql, nameProduct)
+		clothing = Clothes{}
+
+		if err != nil {
+			panic(err.Error())
+		}
+		for rowsQueryClothing.Next() {
+			clothes := Clothing{}
+			rowsQueryClothing.Scan(&clothes.Id, &clothes.Name, &clothes.Breed, &clothes.Cost, &clothes.CustomerCost, &clothes.Existence)
+
+			clothing = append(clothing, clothes)
+			fmt.Println(clothing)
+
+		}
+	}
+
+	return
+}
+
+//FUCT TO DELETE A SPECIFIC CLOHING BY ID
+func DeleteClothing(rw http.ResponseWriter, r *http.Request) {
+	idProduct := r.URL.Query().Get("id")
+	connection := db.OpenConnection()
+	sql := "DELETE FROM productos where idProducts=?"
+	_, err := connection.Exec(sql, idProduct)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println("Producto eliminado")
+	http.Redirect(rw, r, "/seeClothing", http.StatusMovedPermanently)
+}
+
+//FUNCS TO UPDATE A PRODUCT
+func UpdatedClothing(r *http.Request) Clothing {
+	idProduct := r.URL.Query().Get("id")
+	connection := db.OpenConnection()
+	sql := "SELECT * FROM productos where idProducts=?"
+	productQueryClothing, err := connection.Query(sql, idProduct)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	clothes := Clothing{}
+
+	for productQueryClothing.Next() {
+		productQueryClothing.Scan(&clothes.Id, &clothes.Name, &clothes.Breed, &clothes.Cost, &clothes.CustomerCost, &clothes.Existence)
+	}
+	return clothes
+}
+
+func UpdatedClothes(rw http.ResponseWriter, r *http.Request) {
+	connection := db.OpenConnection()
+
+	if r.Method == "POST" {
+
+		idProduct := r.FormValue("id")
+		name := r.FormValue("name")
+		breed := r.FormValue("breed")
+
+		cost, err := strconv.ParseFloat(r.FormValue("cost"), 64)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		customerCost, err := strconv.ParseFloat(r.FormValue("customerCost"), 64)
+		if err != nil {
+			panic(err.Error())
+		}
+		existence, err := strconv.ParseInt(r.FormValue("existence"), 0, 64)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		sql := "UPDATE productos SET name=?, breed=?, cost=?, customerCost=?, existence=? where idProducts=?"
+		responsePrepare, err := connection.Prepare(sql)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		clothes := buildClothing(name, breed, cost, customerCost, existence)
+		_, err = responsePrepare.Exec(&clothes.Name, &clothes.Breed, &clothes.Cost, &clothes.CustomerCost, &clothes.Existence, idProduct)
+		fmt.Println("registro actualizado")
+		if err != nil {
+			panic(err.Error())
+		}
+
+		defer responsePrepare.Close()
+
+	}
+	http.Redirect(rw, r, "/seeClothing", http.StatusMovedPermanently)
 
 }
